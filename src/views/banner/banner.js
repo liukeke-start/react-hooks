4498c9438da276c8991a9ac5940190841574158d
liukeke-start <liukekel@163.com>
2021-12-15 16:25:46 +0800

    添加swiper

--- src/views/banner/banner.js
+++ src/views/banner/banner.js
@@ -1,18 +1,41 @@
-import React,{useState} from "react";
-import style from './index.css';
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
+import 'swiper/swiper.scss'; // core Swiper
+import 'swiper/modules/navigation/navigation.scss'; // Navigation module
+import 'swiper/modules/pagination/pagination.scss'; // Pagination module
 import bannerImg from '@/static/banner.png'
 
+import style from './index.css';
 
+import SwiperCore, {
+  Navigation,Pagination,Autoplay
+} from 'swiper';
+
+const bannerList = [
+  {img:bannerImg},
+  {img:bannerImg},
+  {img:bannerImg},
+]
 const Banner = () => {
-
+  SwiperCore.use([Autoplay,Pagination,Navigation]);
   return (
-    <div className={style.banber}>
-      <div><img src={bannerImg} alt=''/></div>
-      <div><img src={bannerImg} alt=''/></div>
-      <div><img src={bannerImg} alt=''/></div>
+    <div className={style.banner}>
+      <Swiper spaceBetween={30} centeredSlides={true} autoplay={{
+        "delay": 2500,
+        "disableOnInteraction": false
+        }} pagination={{
+        "clickable": true
+        }} navigation={true} 
+        className="mySwiper">
+        {
+          bannerList.map((item,i) => {
+            return (
+              <SwiperSlide key={i}><img src={item.img} alt=''/></SwiperSlide>
+            )
+          })
+        }
+    </Swiper>
     </div>
   )
-
 }
-
 export default Banner
