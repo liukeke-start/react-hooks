0148f6962b4f023fe5fce36d6a41bf8218197fb1
liukeke-start <liukekel@163.com>
2021-12-24 20:02:07 +0800

    渲染接口数据

--- src/views/study/index.js
+++ src/views/study/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getRequest } from '@/libs/api';
 import style from './index.css';
 
+// 接口的参数
 const params = {
   type: 'data',
   channel: 'zaijia_xueersi',
@@ -10,11 +11,12 @@ const params = {
 }
 
 function Study() {
+
   const [datas, setDatas] = useState([]);
   // 请求数据 params 一定要定义值传的参数
   const creat = async () => {
     const result  = await getRequest(params);
-      setDatas(result.data);
+    setDatas(result.data); // 更新datas里面的数据
   }
   useEffect( () => {
     creat();
@@ -22,12 +24,8 @@ function Study() {
   
   return (
     <div className={style.studyBox}>
-      {/* {
-        datas && datas.map((item,index) => (
-          <div>{index}{item.title}</div>
-        ))
-      } */}
       {
+        // 已经是return map函数不用在return 注意是()
         datas && datas.map((item,index) => (
           <div key={index} className={style.studyCon}>
             <a href={item.link}>
@@ -49,38 +47,6 @@ function Study() {
     </div>
   )
 }
+
 export default Study;
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// const params = {
-//   type: 'data',
-//   channel: 'zaijia_xueersi',
-//   catalog: '一年级',
-//   limit: '30'
-// }
-// function Study() {
-//   const [data, setDatat] = useState({ hits: [] });
-
-//   useEffect(async () => {
-//     // const result = await axios(
-//     //   'http://hn.algolia.com/api/v1/search?query=redux',
-//     // );
-//     const result = await getRequest(params);
-//     setDatat(result.data);
-//     console.log(result)
-   
-//   }, []);
-//   // console.log(data)
-//   return (
-//     <ul>
-//       {/* {data.hits.map(item => (
-//         <li key={item.objectID}>
-//           <a href={item.url}>{item.title}</a>
-//         </li>
-//       ))} */}
-//     </ul>
-//   );
-// }
-
-// export default Study;
 
